0f6febbb9d91f715ea0ff5eecf7752d71b93213f
Marius Mitea <mmitea@trusimulation.com>
2020-07-28 11:56:04 -0400

    all navigation keybinds added

--- Pengu/Renderer/UI/AssemblerWindow.cs
+++ Pengu/Renderer/UI/AssemblerWindow.cs
@@ -104,26 +104,7 @@ namespace Pengu.Renderer.UI
             if (key == Keys.Right && action != InputState.Release)
             {
                 if (modifiers.HasFlag(ModifierKeys.Control))
-                {
-                    if (lineCharacterIndex >= lines[verticalOffset + lineIndex].Length)
-                    {
-                        if (verticalOffset + lineIndex < lines.Count - 1)
-                            (lineIndex, lineCharacterIndex) = (lineIndex + 1, lines[verticalOffset + lineIndex + 1].CountWhile(char.IsWhiteSpace));
-                    }
-                    else
-                    {
-                        var line = lines[verticalOffset + lineIndex];
-
-                        // 1. move along either letters/digits or symbols 
-                        if (char.IsLetterOrDigit(line[lineCharacterIndex]))
-                            while (lineCharacterIndex < line.Length && char.IsLetterOrDigit(line[lineCharacterIndex])) ++lineCharacterIndex;
-                        else if (char.IsSymbol(line[lineCharacterIndex]) || char.IsPunctuation(line[lineCharacterIndex]))
-                            while (lineCharacterIndex < line.Length && (char.IsSymbol(line[lineCharacterIndex]) || char.IsPunctuation(line[lineCharacterIndex]))) ++lineCharacterIndex;
-
-                        // 2. move along spaces
-                        while (lineCharacterIndex < line.Length && char.IsWhiteSpace(line[lineCharacterIndex])) ++lineCharacterIndex;
-                    }
-                }
+                    FindNextWord(out lineIndex, out lineCharacterIndex);
                 else if (lineCharacterIndex >= lines[verticalOffset + lineIndex].Length)
                 {
                     if (verticalOffset + lineIndex < lines.Count - 1)
@@ -138,30 +119,7 @@ namespace Pengu.Renderer.UI
             if (key == Keys.Left && action != InputState.Release)
             {
                 if (modifiers.HasFlag(ModifierKeys.Control))
-                {
-                    if (lineCharacterIndex == 0)
-                    {
-                        if (verticalOffset + lineIndex > 0)
-                        {
-                            var previousLine = lines[verticalOffset + lineIndex - 1];
-                            (lineIndex, lineCharacterIndex) = (lineIndex - 1, previousLine.Length - previousLine.CountWhileLast(char.IsWhiteSpace));
-                        }
-                    }
-                    else
-                    {
-                        var line = lines[verticalOffset + lineIndex];
-
-                        // 1. move along spaces
-                        while (lineCharacterIndex > 0 && char.IsWhiteSpace(line[lineCharacterIndex - 1])) --lineCharacterIndex;
-
-                        // 2. move along either letters/digits or symbols 
-                        if (lineCharacterIndex > 0)
-                            if (char.IsLetterOrDigit(line[lineCharacterIndex - 1]))
-                                while (lineCharacterIndex > 0 && char.IsLetterOrDigit(line[lineCharacterIndex - 1])) --lineCharacterIndex;
-                            else if (char.IsSymbol(line[lineCharacterIndex - 1]) || char.IsPunctuation(line[lineCharacterIndex - 1]))
-                                while (lineCharacterIndex > 0 && (char.IsSymbol(line[lineCharacterIndex - 1]) || char.IsPunctuation(line[lineCharacterIndex - 1]))) --lineCharacterIndex;
-                    }
-                }
+                    FindPreviousWord(out lineIndex, out lineCharacterIndex);
                 else if (lineCharacterIndex == 0)
                 {
                     if (verticalOffset + lineIndex > 0)
@@ -201,39 +159,68 @@ namespace Pengu.Renderer.UI
             }
 
             if (key == Keys.Backspace && action != InputState.Release)
-            {
-                var line = lines[verticalOffset + lineIndex];
-                if (lineCharacterIndex > 0)
+                if (modifiers.HasFlag(ModifierKeys.Control))
                 {
-                    lines[verticalOffset + lineIndex] = string.Concat(line.AsSpan(0, lineCharacterIndex - 1), line.AsSpan(lineCharacterIndex--));
-                    syntaxHighlightDirty = contentFontStringDirty = true;
-                }
-                else if (lineIndex + verticalOffset > 0)
-                {
-                    var oldLength = lines[verticalOffset + lineIndex - 1].Length;
-                    lines[verticalOffset + lineIndex - 1] += line;
-                    lines.RemoveAt(verticalOffset + lineIndex);
+                    FindPreviousWord(out var newLineIndex, out var newLineCharacterIndex);
 
-                    (lineIndex, lineCharacterIndex) = (lineIndex - 1, oldLength);
+                    if (newLineIndex != lineIndex)
+                    {
+                        lines[verticalOffset + newLineIndex] = string.Concat(lines[verticalOffset + newLineIndex].AsSpan(0, newLineCharacterIndex), lines[verticalOffset + lineIndex].AsSpan(lineCharacterIndex));
+                        lines.RemoveAt(verticalOffset + lineIndex);
+                    }
+                    else
+                        lines[verticalOffset + lineIndex] = string.Concat(lines[verticalOffset + lineIndex].AsSpan(0, newLineCharacterIndex), lines[verticalOffset + lineIndex].AsSpan(lineCharacterIndex));
+                    (lineIndex, lineCharacterIndex) = (newLineIndex, newLineCharacterIndex);
                     syntaxHighlightDirty = contentFontStringDirty = true;
                 }
-            }
+                else
+                {
+                    var line = lines[verticalOffset + lineIndex];
+                    if (lineCharacterIndex > 0)
+                    {
+                        lines[verticalOffset + lineIndex] = string.Concat(line.AsSpan(0, lineCharacterIndex - 1), line.AsSpan(lineCharacterIndex--));
+                        syntaxHighlightDirty = contentFontStringDirty = true;
+                    }
+                    else if (lineIndex + verticalOffset > 0)
+                    {
+                        var oldLength = lines[verticalOffset + lineIndex - 1].Length;
+                        lines[verticalOffset + lineIndex - 1] += line;
+                        lines.RemoveAt(verticalOffset + lineIndex);
+
+                        (lineIndex, lineCharacterIndex) = (lineIndex - 1, oldLength);
+                        syntaxHighlightDirty = contentFontStringDirty = true;
+                    }
+                }
 
             if (key == Keys.Delete && action != InputState.Release)
-            {
-                var line = lines[verticalOffset + lineIndex];
-                if (lineCharacterIndex < line.Length)
+                if (modifiers.HasFlag(ModifierKeys.Control))
                 {
-                    lines[verticalOffset + lineIndex] = string.Concat(line.AsSpan(0, lineCharacterIndex), line.AsSpan(lineCharacterIndex + 1));
+                    FindNextWord(out var newLineIndex, out var newLineCharacterIndex);
+
+                    if (newLineIndex != lineIndex)
+                    {
+                        lines[verticalOffset + lineIndex] = string.Concat(lines[verticalOffset + lineIndex].AsSpan(0, lineCharacterIndex), lines[verticalOffset + newLineIndex].AsSpan(newLineCharacterIndex));
+                        lines.RemoveAt(verticalOffset + newLineIndex);
+                    }
+                    else
+                        lines[verticalOffset + lineIndex] = string.Concat(lines[verticalOffset + lineIndex].AsSpan(0, lineCharacterIndex), lines[verticalOffset + lineIndex].AsSpan(newLineCharacterIndex));
                     syntaxHighlightDirty = contentFontStringDirty = true;
                 }
-                else if (lineIndex + verticalOffset < lines.Count - 1)
+                else
                 {
-                    lines[verticalOffset + lineIndex] += lines[verticalOffset + lineIndex + 1];
-                    lines.RemoveAt(verticalOffset + lineIndex + 1);
-                    syntaxHighlightDirty = contentFontStringDirty = true;
+                    var line = lines[verticalOffset + lineIndex];
+                    if (lineCharacterIndex < line.Length)
+                    {
+                        lines[verticalOffset + lineIndex] = string.Concat(line.AsSpan(0, lineCharacterIndex), line.AsSpan(lineCharacterIndex + 1));
+                        syntaxHighlightDirty = contentFontStringDirty = true;
+                    }
+                    else if (lineIndex + verticalOffset < lines.Count - 1)
+                    {
+                        lines[verticalOffset + lineIndex] += lines[verticalOffset + lineIndex + 1];
+                        lines.RemoveAt(verticalOffset + lineIndex + 1);
+                        syntaxHighlightDirty = contentFontStringDirty = true;
+                    }
                 }
-            }
 
             if (key == Keys.Home && action == InputState.Press && lines.Any())
             {
@@ -253,16 +240,28 @@ namespace Pengu.Renderer.UI
                 syntaxHighlightDirty = contentFontStringDirty = true;
             }
 
+            if (key == Keys.PageUp && action != InputState.Release && lineIndex + verticalOffset > 0)
+            {
+                lineIndex = Math.Max(-verticalOffset, lineIndex - editorLineRows);
+                syntaxHighlightDirty = contentFontStringDirty = true;
+            }
+
+            if (key == Keys.PageDown && action != InputState.Release && lineIndex + verticalOffset < lines.Count - 1)
+            {
+                lineIndex = Math.Min(lines.Count - 1 - verticalOffset, lineIndex + editorLineRows);
+                syntaxHighlightDirty = contentFontStringDirty = true;
+            }
+
             // ensure cursor visible
             if (lineIndex < 0)
             {
                 (lineIndex, verticalOffset) = (0, verticalOffset + lineIndex);
-                syntaxHighlightDirty = true;
+                contentFontStringDirty = syntaxHighlightDirty = true;
             }
             else if (lineIndex >= Height)
             {
                 (lineIndex, verticalOffset) = (Height - 1, verticalOffset + lineIndex - Height + 1);
-                syntaxHighlightDirty = true;
+                contentFontStringDirty = syntaxHighlightDirty = true;
             }
 
             if (key == Keys.F6 && action == InputState.Press)
@@ -275,6 +274,58 @@ namespace Pengu.Renderer.UI
             }
 
             return false;
+
+            void FindPreviousWord(out int newLineIndex, out int newLineCharacterIndex)
+            {
+                (newLineIndex, newLineCharacterIndex) = (lineIndex, lineCharacterIndex);
+
+                if (lineCharacterIndex == 0)
+                {
+                    if (verticalOffset + lineIndex > 0)
+                    {
+                        var previousLine = lines[verticalOffset + lineIndex - 1];
+                        (newLineIndex, newLineCharacterIndex) = (lineIndex - 1, previousLine.Length - previousLine.CountWhileLast(char.IsWhiteSpace));
+                    }
+                }
+                else
+                {
+                    var line = lines[verticalOffset + lineIndex];
+
+                    // 1. move along spaces
+                    while (newLineCharacterIndex > 0 && char.IsWhiteSpace(line[newLineCharacterIndex - 1])) --newLineCharacterIndex;
+
+                    // 2. move along either letters/digits or symbols 
+                    if (newLineCharacterIndex > 0)
+                        if (char.IsLetterOrDigit(line[newLineCharacterIndex - 1]))
+                            while (newLineCharacterIndex > 0 && char.IsLetterOrDigit(line[newLineCharacterIndex - 1])) --newLineCharacterIndex;
+                        else if (char.IsSymbol(line[newLineCharacterIndex - 1]) || char.IsPunctuation(line[newLineCharacterIndex - 1]))
+                            while (newLineCharacterIndex > 0 && (char.IsSymbol(line[newLineCharacterIndex - 1]) || char.IsPunctuation(line[newLineCharacterIndex - 1]))) --newLineCharacterIndex;
+                }
+            }
+
+            void FindNextWord(out int newLineIndex, out int newLineCharacterIndex)
+            {
+                (newLineIndex, newLineCharacterIndex) = (lineIndex, lineCharacterIndex);
+
+                if (lineCharacterIndex >= lines[verticalOffset + lineIndex].Length)
+                {
+                    if (verticalOffset + lineIndex < lines.Count - 1)
+                        (newLineIndex, newLineCharacterIndex) = (lineIndex + 1, lines[verticalOffset + lineIndex + 1].CountWhile(char.IsWhiteSpace));
+                }
+                else
+                {
+                    var line = lines[verticalOffset + lineIndex];
+
+                    // 1. move along either letters/digits or symbols 
+                    if (char.IsLetterOrDigit(line[newLineCharacterIndex]))
+                        while (newLineCharacterIndex < line.Length && char.IsLetterOrDigit(line[newLineCharacterIndex])) ++newLineCharacterIndex;
+                    else if (char.IsSymbol(line[newLineCharacterIndex]) || char.IsPunctuation(line[newLineCharacterIndex]))
+                        while (newLineCharacterIndex < line.Length && (char.IsSymbol(line[newLineCharacterIndex]) || char.IsPunctuation(line[newLineCharacterIndex]))) ++newLineCharacterIndex;
+
+                    // 2. move along spaces
+                    while (newLineCharacterIndex < line.Length && char.IsWhiteSpace(line[newLineCharacterIndex])) ++newLineCharacterIndex;
+                }
+            }
         }
 
         public override void UpdateLogic(TimeSpan elapsedTime) { }
